258eef15856ad191dcab2e7317795e7650ad77d5
Nam <youarebendano@gmail.com>
2026-02-25 09:28:08 +0800

    reset configs

--- lua/configs/keymaps.lua
+++
@@ -1,8 +0,0 @@
-local map = vim.keymap.set
-local map_opts = {
-  noremap = true,
-  silent = true,
-}
-
-
--- map('n', '-', '<CMD>Rexplore<CR>', { desc = 'Open Parent Directory' })
--- lua/configs/lsp.lua
+++
@@ -1,5 +0,0 @@
-vim.lsp.enable({
-	'lua_ls',
-	'ruff',
-	'ty',
-})
